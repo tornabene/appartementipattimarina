0064ff8bf341b7937e70df25500ceddd059c9081
tindaro tornabene <tindaro.tornabene@ietservizi.it>
2014-07-08 23:36:31 +0200

    add

--- app/scripts/controllers/vacanza.js
+++ app/scripts/controllers/vacanza.js
@@ -6,13 +6,13 @@ angular.module('appartamentipattimarinaApp')
 	$scope.photos = [
 		{src: '/images/galleria/gazebo.jpg', desc: 'Gazebo e piscina'},
 		{src: '/images/galleria/angolo-cottura.jpg', desc: 'Angolo cottura'},
-		{src: '/home/tino/Desktop/appa/bagno.jpg', desc: 'Bagno doccia'},
+		{src: '/images/galleria/bagno.jpg', desc: 'Bagno doccia'},
 		{src: '/images/galleria/cameretta.jpg', desc: 'Cameretta'},
 		{src: '/images/galleria/piscina.jpg', desc: 'Piscina'},
 		{src: 'images/galleria/salone-cucina.jpg', desc: 'Salone cucina'},
 		{src: '/images/galleria/stanxa-da-letto.jpg', desc: 'Stanza da letto matrimoniale'},
 		{src: '/images/galleria/terrazzo.jpg', desc: 'Terrazzo'},
-		{src: '/home/tino/Desktop/appa/soggiorno.jpg', desc: 'Soggiorno'}
+		{src: '/images/galleria/soggiorno.jpg', desc: 'Soggiorno'}
 
 	];
 	
