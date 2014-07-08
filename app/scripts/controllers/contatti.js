614462d72cdcbe604bac5a733af100e95b945ac2
tindaro tornabene <tindaro.tornabene@ietservizi.it>
2014-07-08 23:25:29 +0200

    add shapshot

--- app/scripts/controllers/contatti.js
+++ app/scripts/controllers/contatti.js
@@ -7,47 +7,45 @@ angular.module('appartamentipattimarinaApp')
 	$scope.httpError = false;
 
 
-$scope.email = {  };
+	$scope.email = {  };
 	$scope.send = function() {
 
-var testo = "Appartamenti patti marina\n Nome:"+$scope.contact.nome +	"\n Cognome:"+$scope.contact.cognome+	"\n Email: "+$scope.contact.email+"\n Città:"+$scope.contact.citta+		"\n NumeroOspiti:"+$scope.contact.numeroOspiti+	"\n messagio:"+$scope.contact.messagio;
-var subject =  "Appartamenti patti marina Info - "+$scope.contact.nome + " "+$scope.contact.nome;
-var name = $scope.contact.nome + " "+$scope.contact.cognome;
+		var testo = 'Appartamenti patti marina\n Nome:'+$scope.contact.nome +	'\n Cognome:'+$scope.contact.cognome+	'\n Email: '+$scope.contact.email+'\n Città:'+$scope.contact.citta+		'\n NumeroOspiti:'+$scope.contact.numeroOspiti+	'\n messagio:'+$scope.contact.messagio;
+		var subject =  'Appartamenti patti marina Info - '+$scope.contact.nome + ' '+$scope.contact.nome;
+		var name = $scope.contact.nome + ' '+$scope.contact.cognome;
 
 
-		$scope.email = {     "key": "3MK6koq2JE5q75xytmAKag",
-		"message": {
-			"text": testo ,	
-			"subject": subject,	
-			"from_email": $scope.contact.email,	
-			"from_name": name ,
-
-			"to": [
- 
-			{
-				"email": "segreto.nicolo@gmail.com",
-				"name": "Segreto Nicolo",
-				"type": "to"
+		$scope.email = {
+			'key': '3MK6koq2JE5q75xytmAKag',
+			'message': {
+				'text' : testo,
+				'subject' : subject,
+				'from_email' : $scope.contact.email,
+				'from_name': name,
+				'to': [
+					{
+						'email' : 'segreto.nicolo@gmail.com',
+						'name' : 'Segreto Nicolo',
+						'type' : 'to'
+					}
+				]
 			}
-			]
+		};
 
-		}
+		MailService.send($scope.email ,
+			function(data){
+				$scope.success = true;
+				$scope.msg = {data:data};
+			},
+			function(data){
+				$scope.httpError = true;
+				$scope.error = {data:data};
+			});
 	};
 
-	MailService.send($scope.email ,
-		function(data){
-			$scope.success = true;
-			$scope.msg = {data:data};
-		},
-		function(data){
-			$scope.httpError = true;
-			$scope.error = {data:data};
-		});
-};
+	$rootScope.title = 'Contatti Appartamenti';
+	$rootScope.showCaousel = false;
 
-$rootScope.title = 'Contatti Appartamenti';
-$rootScope.showCaousel = false;
-
-$rootScope.status = 'ready';
+	$rootScope.status = 'ready';
 
 });
